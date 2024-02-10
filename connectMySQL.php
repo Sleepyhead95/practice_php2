28647290bcab3112fe8b0cb50a882a98f4c02cde
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-02-10 11:00:17 +0100

    insert data into MySQL table using php code

--- connectMySQL.php
+++ connectMySQL.php
@@ -1,5 +1,28 @@
 <?php
 include("database.php"); // to connect this page to the DB
+
+// we can also insert variables into the table
+$username = "Patrick";
+$password = "rock3";
+
+// to hash our password, we use the password hash func & a default algorithm
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+// this query inserts a new row/user into our users table
+
+$sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+
+// if any of the required values is missing, the query will fail
+// try/catch block to handle errors
+try {
+    mysqli_query($connection, $sql); // to run the query (inserting a new user into the table)
+    echo "New user added <br>";
+} catch (mysqli_sql_exception) {
+    echo "Could not register user";
+}
+
+
+mysqli_close($connection); // to close the connection
 ?>
 
 <!DOCTYPE html>
